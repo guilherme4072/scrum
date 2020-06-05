ac6006093fed7953db4b25ecda0cc188c343170c
Guilherme Cordeiro <guilherme4072@gmail.com>
2020-06-05 14:03:37 -0300

    Plan Corrected

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,16 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    protected $table = 'plans';
-    protected $fillable = [
-        'name',
-        'description',
-        'price',
-        'ative'
-    ];
-
-    protected $hidden = [
-        'created_at',
-        'updated_at'
-    ];
 }
